618a92e36b4749578e34f0e2dcda36460eea2eea
10sr <8.slashes@gmail.com>
2019-04-23 13:44:58 +0900

    Add some views

--- trigger/urls.py
+++ trigger/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("note/", RedirectView.as_view(url="from/web"), name="note"),
+    path("note/from/web", views.note_from_web, name="note_from_web"),
 ]
--- trigger/views.py
+++ trigger/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(req):
-    return HttpResponse("hello")
+    return HttpResponse("""
+    hello
+    <a href="note">note</a>
+    """)
+
+
+def note_from_web(req):
+    return HttpResponse("hoehoe")
